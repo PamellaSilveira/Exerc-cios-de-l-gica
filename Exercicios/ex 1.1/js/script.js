e6a15383f82b4d46154b2bc6eb99b954f57f9923
Pamella Silveira <oliveirapamella51@gmail.com>
2025-08-14 19:46:19 -0300

    first commit

---
+++ Exercicios/ex 1.1/js/script.js
@@ -0,0 +1,12 @@
+/*
+Exercício 1.1
+Faça um programa que exiba 3 números na tela.
+
+Dica - exibindo (ou "imprimindo") números na tela:
+*/
+
+
+const num1=1;
+const num2="2";
+
+console.info(num1+num2);
---
+++ Exercicios/ex 2.1/js/script.js
@@ -0,0 +1,26 @@
+/*
+Exercício 2.1
+Faça um programa que leia 2 números, some-os e exiba 
+uma mensagem com o resultado apenas se o resultado 
+for maior que 100.
+*/
+
+var num1 = parseFloat(prompt("Digite um número: "));
+var num2 = parseFloat(prompt("Digite mais um número: "));
+
+soma = num1 + num2;
+numLimite = 100;
+
+
+soma > 100 ? alert (`A soma dos números é ${soma}, que é maior que 100.`)
+:alert(`A soma dos números é ${soma}, que é maior ou igual a 100`);
+
+console.log ("Números somandos:", num1, num2);
+console.log("Resultado:", soma)
+
+//if (soma > numLimite) 
+//{
+  //  alert(`A soma dos dois numeros é: ${soma}`);
+   // console.log("Números somados:", num1, num2);
+  //  console.log("Resultado:",soma);
+//}
